10f2956eaea36b7a6c59031a6c4caf0079ec9731
stephen-leisy <stleisy@gmail.com>
2021-01-12 13:29:35 -0800

    set up html framework and grabbed DOM elements + tested.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // import functions and grab DOM elements
+const userGuess = document.getElementById('guess-input');
+const magicButton = document.getElementById('magic-button');
+const guessesLeft = document.getElementById('remaining-guesses');
+const showResults = document.getElementById('feedback-window');
 
+console.log(userGuess, magicButton, guessesLeft, showResults);
 // initialize state
 
 // set event listeners to update state and DOM
